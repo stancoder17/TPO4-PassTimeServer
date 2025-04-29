1ea2bef02c16d9526a7f5774414833a7df92f6c3
stani <stanislawdyrda7@gmail.com>
2025-04-29 12:29:17 +0200

    Implement ClientTask

--- TPO4_DS_S31552/src/zad1/ClientTask.java
+++ TPO4_DS_S31552/src/zad1/ClientTask.java
@@ -6,6 +6,25 @@
 
 package zad1;
 
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
-public class ClientTask {
+public class ClientTask extends FutureTask<String> {
+
+    private ClientTask(Callable<String> task) {
+        super(task);
+    }
+
+    public static ClientTask create(Client client, List<String> reqList, boolean showRes) {
+        return new ClientTask(() -> {
+            client.connect();
+            client.send("login " + client.getId());
+            for (String req : reqList) {
+                String res = client.send(req);
+                if (showRes) System.out.println(res);
+            }
+            return client.send("bye and log transfer");
+        });
+    }
 }
