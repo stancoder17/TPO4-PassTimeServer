d7d6054a514aea8c082dd0f0c4416e8b899c2353
stani <stanislawdyrda7@gmail.com>
2025-04-29 12:28:04 +0200

    Make Client wait if Server not yet opened, fix Client logging

--- TPO4_DS_S31552/src/zad1/Client.java
+++ TPO4_DS_S31552/src/zad1/Client.java
@@ -20,8 +20,9 @@ import java.util.Set;
 public class Client {
     private final String host;
     private final int port;
-    private final String id;
     private SocketChannel sc;
+    private final String id;
+    private final Log log = new Log();
 
     public Client(String host, int port, String id) {
         this.host = host;
@@ -30,24 +31,39 @@ public class Client {
     }
 
     public void connect() {
-        try {
-            sc = SocketChannel.open();
-            sc.configureBlocking(false);
-            sc.connect(new InetSocketAddress(host, port)); // this makes key.isAcceptable = true
-            while (!sc.finishConnect()) {
-                Thread.sleep(100);
+        log.add("=== " + id + " log start ===");
+
+        int attempts = 10;
+
+        while (attempts-- > 0) {
+            try {
+                sc = SocketChannel.open();
+                sc.configureBlocking(false);
+                sc.connect(new InetSocketAddress(host, port));
+
+                int count = 0; // to make sure that the while loop stops
+                while (!sc.finishConnect() && count < 50) { // 5 seconds in that case
+                    Thread.sleep(100);
+                    count++;
+                }
+
+                return;
+            } catch (IOException | InterruptedException e) {
+                if (attempts == 0) {
+                    throw new RuntimeException(e);
+                }
+                try { // If server not yet ready
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {
+                }
             }
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 
     public String send(String req) {
         try {
             // Writing
-            req += "\n";
-
-            ByteBuffer buf = ByteBuffer.wrap(req.getBytes(StandardCharsets.UTF_8));
+            ByteBuffer buf = ByteBuffer.wrap((req + "\n").getBytes(StandardCharsets.UTF_8));
 
             while (buf.hasRemaining()) {
                 sc.write(buf);
@@ -72,10 +88,31 @@ public class Client {
                     sb.append(BufferOperations.readMessage(serverChannel));
                 }
             }
+            String resp = sb.toString();
 
-            return sb.toString();
+            // Logging
+            String[] tokens = req.split(" ");
+            boolean isDateRequest = !tokens[0].equals("login") && !tokens[0].equals("bye");
+
+            if (isDateRequest) {
+                log.add("Request: " + req);
+                log.add("Result:\n" + resp);
+            } else {
+                log.add(resp);
+            }
+
+
+            if (req.equals("bye and log transfer")) {
+                log.add("=== " + id + " log end ===");
+                return log.getLog();
+            }
+            return isDateRequest ? resp : "";
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getId() {
+        return id;
     }
 }
